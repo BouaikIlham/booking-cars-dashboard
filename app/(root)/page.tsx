fd05bcaa98251ea59c9f34ce9958169b344a5b54
ilham <ibouaik@gmail.com>
2023-07-10 23:52:59 +0100

    fix haydration error

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,9 @@
 "use client"
-import { StoreModal } from "@/components/store-modal";
 
 const SetupPage = () => {
   return (
    <div className="p-4">
-     <StoreModal/>
+      Root Page
    </div>
   );
 };
