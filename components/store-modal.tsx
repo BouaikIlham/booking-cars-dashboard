d0a614922efc1b70abcc129bc49cbb651c4c0682
ilham <ibouaik@gmail.com>
2023-07-11 09:48:36 +0100

    Define fomr

--- components/store-modal.tsx
+++ components/store-modal.tsx
@@ -1,19 +1,35 @@
-"use client"
+"use client";
 
-import { useStoreModal } from "@/hooks/useStoreModal"
-import { Modal } from "../components/ui/Modal"
+import { useStoreModal } from "@/hooks/useStoreModal";
+import { Modal } from "../components/ui/Modal";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
 
 export const StoreModal = () => {
-    const storeModal = useStoreModal();
+  const storeModal = useStoreModal();
 
-    return (
-        <Modal 
-            title="Create store"
-            description="Add a new store to manage cars"
-            isOpen={storeModal.isOpen}
-            onClose={storeModal.onClose}
-        >
-            Future Create Store Form
-        </Modal>
-    )
-}
+  const formSchema = z.object({
+    name: z.string().min(1),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+        name: "",
+    },
+  });
+
+  console.log(form)
+
+  return (
+    <Modal
+      title="Create store"
+      description="Add a new store to manage cars"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      Future Create Store Form
+    </Modal>
+  );
+};
