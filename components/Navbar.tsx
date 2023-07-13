8f5a85d36242efce96825488bc80b4158176e95b
ilham <ibouaik@gmail.com>
2023-07-13 23:34:02 +0100

    navbar structure

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,24 @@
+"use client"
+
+import { UserButton } from "@clerk/nextjs"
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <>
+      <div className="border-b">
+        <div className="h-16 px-4 flex items-center">
+          <div>
+           this will be store switcher
+          </div>
+          <div>
+            this will be routes
+          </div>
+          <div className="flex items-center ml-auto space-x-4">
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
