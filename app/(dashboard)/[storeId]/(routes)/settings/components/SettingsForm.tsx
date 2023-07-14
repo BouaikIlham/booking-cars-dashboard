c7be3f2d33c8a998cf3e1b61f96d4076d179c04a
ilham <ibouaik@gmail.com>
2023-07-14 23:19:18 +0100

    update store

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -1,16 +1,16 @@
-"use client"
- 
+"use client";
+
 import { useState } from "react";
-import { Store } from "@prisma/client"
+import { Store } from "@prisma/client";
 import Heading from "@/components/ui/Heading";
 import { Button } from "@/components/ui/button";
 import { Trash } from "lucide-react";
-import { Separator } from "@/components/ui/separator"
+import { Separator } from "@/components/ui/separator";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod"
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
-
+import { toast } from "react-hot-toast";
 import {
   Form,
   FormControl,
@@ -18,51 +18,60 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
- 
- interface SettingsFormProps {
-    initialData: Store;
- }
+} from "@/components/ui/form";
+import axios from "axios";
+import { useParams, useRouter } from "next/navigation";
 
- const formSchema = z.object({
+interface SettingsFormProps {
+  initialData: Store;
+}
+
+const formSchema = z.object({
   name: z.string().min(1),
 });
 
-
-const SettingsForm: React.FC<SettingsFormProps> = ({
-    initialData
-}) => {
-
+const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
+  const params = useParams();
+  const router = useRouter();
   const [open, setOpen] = useState(false);
-  const [loading, setIsLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues:  initialData
+    defaultValues: initialData,
   });
 
   const onSubmit = async (data: z.infer<typeof formSchema>) => {
-    console.log(data)
-  }
+    try {
+      setLoading(true);
+      await axios.patch(`/api/stores/${params.storeId}`, data);
+      toast.success("store updated")
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong!");
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <>
       <div className="flex items-center justify-between">
-        <Heading
-          title="Settings"
-          description="Manage store preferences"
-        />
+        <Heading title="Settings" description="Manage store preferences" />
         <Button
           variant="destructive"
           disabled={loading}
           size="icon"
           onClick={() => setOpen(true)}
         >
-        <Trash className="h-4 w-4"/>
+          <Trash className="h-4 w-4" />
         </Button>
       </div>
       <Separator />
       <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 w-full">
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 w-full"
+        >
           <div className=" grid grid-cols-3 gap-3">
             <FormField
               control={form.control}
@@ -70,21 +79,25 @@ const SettingsForm: React.FC<SettingsFormProps> = ({
               render={({ field }) => (
                 <FormItem>
                   <FormLabel>Name</FormLabel>
-                    <FormControl>
-                        <Input disabled={loading} placeholder="Store name" {...field} />
-                    </FormControl>
-                    <FormMessage />
+                  <FormControl>
+                    <Input
+                      disabled={loading}
+                      placeholder="Store name"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
                 </FormItem>
               )}
             />
           </div>
           <Button disabled={loading} className="ml-auto" type="submit">
-                Save changes
+            Save changes
           </Button>
         </form>
       </Form>
     </>
-  )
-}
+  );
+};
 
-export default SettingsForm
+export default SettingsForm;
