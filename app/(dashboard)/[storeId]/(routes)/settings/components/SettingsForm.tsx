003724ba8142783ff524c1bfbf0213f3d3d2784f
ilham <ibouaik@gmail.com>
2023-07-14 18:40:41 +0100

    add heading and trash button to settings page

--- app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/SettingsForm.tsx
@@ -1,15 +1,36 @@
 "use client"
 
 import { Store } from "@prisma/client"
+import Heading from "@/components/ui/Heading";
+import { Button } from "@/components/ui/button";
+import { Trash } from "lucide-react";
+import { Separator } from "@/components/ui/separator"
 
  interface SettingsFormProps {
     initialData: Store;
  }
+
+ 
 const SettingsForm: React.FC<SettingsFormProps> = ({
     initialData
 }) => {
   return (
-    <div>SettingsForm</div>
+    <>
+      <div className="flex items-center justify-between">
+        <Heading
+          title="Settings"
+          description="Manage store preferences"
+        />
+        <Button
+          variant="destructive"
+          size="icon"
+          onClick={() => {}}
+        >
+        <Trash className="h-4 w-4"/>
+        </Button>
+      </div>
+      <Separator />
+    </>
   )
 }
 
