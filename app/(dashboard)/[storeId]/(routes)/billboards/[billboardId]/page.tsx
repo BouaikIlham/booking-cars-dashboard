fc7eba590d4b3dfee5222565f5374a325e9115ac
ilham <ibouaik@gmail.com>
2023-07-15 12:43:38 +0100

    create billboards page and single billboard page

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/[billboardId]/page.tsx
@@ -0,0 +1,17 @@
+
+
+
+
+const Billboard = async ({
+    params
+}: {
+    params: {billboardId: string}
+}) => {
+    return (
+        <div>
+            this is  a form for  billboards
+        </div>
+    )
+}
+
+export default Billboard; 
